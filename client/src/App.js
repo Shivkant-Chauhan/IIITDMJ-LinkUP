e29fb547ce70b63a5d178d947a7a079844cda7db
Shivkant Chauhan <shivkantchauhan2002@gmail.com>
2022-12-18 02:58:25 +0530

    working on components

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Form,
+} from 'react-router-dom';
+
+// components
+import User from './components/user-profile/User';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* Bootstrap and Font-Awesome CDNs */}
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+      
+
+      <div className="App">
+
+        {/* NAVBAR always displayed! */}
+
+        <Routes>
+          {/* <Route exact path="/" element={<Home />} /> */}
+          <Route exact path="/user" element={<User />} />
+        </Routes>
+        
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/components/user-profile/User.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./user.scss";
+
+function User() {
+  return (
+    <div className="user">
+      <div className="profile">
+        <div className="user-img"></div>
+        <div className="user-details"></div>
+      </div>
+    </div>
+  );
+}
+
+export default User;
