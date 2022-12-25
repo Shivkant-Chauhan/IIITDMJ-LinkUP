674698c5ddbc5543a906465abe60cdb39b738d9d
Shivkant Chauhan <91013793+Shivkant-Chauhan@users.noreply.github.com>
2022-12-25 22:23:17 +0530

    Update faculty.jsx

--- client/src/components/IIITDMJ/component/faculty.jsx
+++ client/src/components/IIITDMJ/component/faculty.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./style.css"
 export default function Faculty() {
-    return (
-        <div>
-        <h1>Faculty</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Faculty</h1>
+    </div>
+  );
 }
